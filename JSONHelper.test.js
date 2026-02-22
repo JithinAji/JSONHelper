6beb15e74a115268d29fd5d6ae5f04a6968daa21
Jithin Aji <jithinaji@Jithins-MacBook-Air.local>
2026-02-22 22:42:58 +0530

    Add tests
    - undo/redo
    - nested deleteKey and set

--- JSONHelper.test.js
+++ JSONHelper.test.js
@@ -1,15 +1,44 @@
-import {describe, it, expect} from "vitest";
+import {describe, it, expect, beforeEach} from "vitest";
 import createJSONEngine from "./JSONHelper.js";
 
 describe("JSONEngine", () => {
+  let engine;
+  beforeEach(() => {
+    engine = createJSONEngine({a: 1});
+  })
+
   it("returns initial value via get", () => {
-    const engine = createJSONEngine({a: 1});
+    const data = engine.getData();
+    data.a = 0;
     expect(engine.get("a")).toBe(1);
   });
 
   it("adds a new key using set", () => {
-    const engine = createJSONEngine({a: 1});
     engine.set("b", 2);
     expect(engine.get("b")).toBe(2);
   });
+
+  it("gets correct value using getData", () => {
+    expect(engine.getData()).toEqual({a: 1});
+  });
+
+  it("deletes and add nested keys", () => {
+    engine.set("b.c.d", 3);
+    expect(engine.get("b.c.d")).toBe(3);
+    engine.deleteKey("b.c.d");
+    expect(engine.getData()).toEqual({a: 1, b: {c: {}}});
+  });
+
+  it("undo reverts last change", () => {
+    engine.set("b", 2);
+    engine.undo();
+    expect(() => engine.get("b")).toThrow();
+  });
+
+  it("redo reverts undo", () => {
+    engine.set("b", 2);
+    engine.undo();
+    engine.redo();
+    expect(engine.get("b")).toBe(2);
+  })
 })
