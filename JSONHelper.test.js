8b16dd317c06cf68bc36f188ec64778f71abacd3
Jithin Aji <jithinaji@Jithins-MacBook-Air.local>
2026-02-22 23:38:37 +0530

    Cleaner and grouped test cases

--- JSONHelper.test.js
+++ JSONHelper.test.js
@@ -1,44 +1,70 @@
-import {describe, it, expect, beforeEach} from "vitest";
+import {describe, it, expect, beforeEach, vi} from "vitest";
 import createJSONEngine from "./JSONHelper.js";
 
 describe("JSONEngine", () => {
   let engine;
+
   beforeEach(() => {
     engine = createJSONEngine({a: 1});
-  })
-
-  it("returns initial value via get", () => {
-    const data = engine.getData();
-    data.a = 0;
-    expect(engine.get("a")).toBe(1);
   });
 
-  it("adds a new key using set", () => {
-    engine.set("b", 2);
-    expect(engine.get("b")).toBe(2);
+  describe("Get", () => {
+
+    it("getData returns a copy and does not mutate internal state", () => {
+     const data = engine.getData();
+      data.a = 0;
+      expect(engine.get("a")).toBe(1);
+    });
+
+    it("gets correct value using getData", () => {
+      expect(engine.getData()).toEqual({a: 1});
+    });
+
   });
 
-  it("gets correct value using getData", () => {
-    expect(engine.getData()).toEqual({a: 1});
+  describe("Mutation",() => {
+    it("adds a new key using set", () => {
+      engine.set("b", 2);
+      expect(engine.get("b")).toBe(2);
+    });
+
+
+    it("deletes and add nested keys", () => {
+      engine.set("b.c.d", 3);
+      expect(engine.get("b.c.d")).toBe(3);
+      engine.deleteKey("b.c.d");
+      expect(engine.getData()).toEqual({a: 1, b: {c: {}}});
+    });
   });
 
-  it("deletes and add nested keys", () => {
-    engine.set("b.c.d", 3);
-    expect(engine.get("b.c.d")).toBe(3);
-    engine.deleteKey("b.c.d");
-    expect(engine.getData()).toEqual({a: 1, b: {c: {}}});
+  describe("Undo Redo", () => {
+    it("undo reverts last change", () => {
+      engine.set("b", 2);
+      engine.undo();
+      expect(() => engine.get("b")).toThrow();
+    });
+
+    it("redo reverts undo", () => {
+      engine.set("b", 2);
+      engine.undo();
+      engine.redo();
+      expect(engine.get("b")).toBe(2);
+    });
+
+    it("does not create history for identical values", () => {
+      engine.set("a", 1);
+      engine.undo();
+      expect(engine.get("a")).toBe(1);
+    })
   });
 
-  it("undo reverts last change", () => {
-    engine.set("b", 2);
-    engine.undo();
-    expect(() => engine.get("b")).toThrow();
-  });
 
-  it("redo reverts undo", () => {
-    engine.set("b", 2);
-    engine.undo();
-    engine.redo();
-    expect(engine.get("b")).toBe(2);
-  })
+  describe("Listeners", () => {
+    it("notifies listeners on change", () => {
+      const fn = vi.fn();
+      engine.onChange(fn);
+      engine.set("b", 2);
+      expect(fn).toHaveBeenCalled();
+    })
+  }) 
 })
